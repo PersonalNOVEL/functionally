dfb07c93688546e95b9657b489eb57475428925f
Daniel Werner <daniel.d.werner@googlemail.com>
2012-07-31 09:52:15 +0200

    Added setup.py keywords, classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
 
 setup(name='functionally',
       version='1.0',
       description='Simple & extensive functional programming library',
+      long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
       author='Daniel Werner',
       author_email='dwerner@personalnovel.de',
       url='http://github.com/PersonalNOVEL/functionally',
       packages=['functionally'],
+      keywords="functional",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Utilities",
+      ],
       license="MIT",
      )
